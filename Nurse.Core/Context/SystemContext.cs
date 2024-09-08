d77786978785ba18332aed7fe0f654c4d38d3bc1
Ethan Phelps <60406674+felpsey@users.noreply.github.com>
2024-09-08 22:42:17 +0100

    Created a table to list system context information and begun populating with system information

--- Nurse.Core/Context/SystemContext.cs
+++ Nurse.Core/Context/SystemContext.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Nurse.Core.SystemContext
+namespace Nurse.Core.Context
 {
   public abstract class SystemContext
   {
--- Nurse.Desktop/Views/SystemPage.xaml.cs
+++ Nurse.Desktop/Views/SystemPage.xaml.cs
@@ -6,13 +6,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Nurse.Core.Context;
+
 namespace Nurse.Desktop.Views
 {
 	public sealed partial class SystemPage : Page
 	{
 		public SystemPage()
 		{
+			this._OperatingSystemContext = new OperatingSystemContext();
+
+			this._Hostname = _OperatingSystemContext.Hostname;
+
 			this.InitializeComponent();
+		}
+
+		private OperatingSystemContext _OperatingSystemContext;
+		
+		private readonly string _Hostname;
+
+		public string Hostname
+		{
+			get
+			{
+				return this._Hostname;
+			}
 		}
 	}
 }
