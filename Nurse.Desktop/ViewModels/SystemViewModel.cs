b0c24d951cb971d62bbfde11308caeb16a8bbf1d
Ethan Phelps <60406674+felpsey@users.noreply.github.com>
2024-09-09 22:59:17 +0100

    Aligned application architecture to align with MVVM

---
+++ Nurse.Desktop/ViewModels/SystemViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Nurse.Core.Context;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nurse.Desktop.ViewModels
+{
+	public class SystemViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private OperatingSystemContext _OperatingSystemContext;
+
+		public SystemViewModel()
+		{
+			_OperatingSystemContext = new OperatingSystemContext();
+
+			Hostname = _OperatingSystemContext.Hostname;
+			WindowsVersion = _OperatingSystemContext.WindowsVersion;
+			WindowsProductType = _OperatingSystemContext.WindowsProductType;
+			WindowsDomainName = _OperatingSystemContext.WindowsDomainName;
+		}
+
+		public string Hostname { get; }
+		public string WindowsVersion { get; }
+		public string WindowsProductType { get; }
+		public string WindowsDomainName { get; }
+
+		// Notify the view when a property changes
+		protected void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
--- Nurse.Desktop/Views/SystemPage.xaml.cs
+++ Nurse.Desktop/Views/SystemPage.xaml.cs
@@ -7,36 +7,19 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Nurse.Core.Context;
+using Nurse.Desktop.ViewModels;
 
 namespace Nurse.Desktop.Views
 {
 	public sealed partial class SystemPage : Page
 	{
-		private OperatingSystemContext _OperatingSystemContext;
-
-		private readonly string _Hostname;
-		private readonly string _WindowsVersion;
-		private readonly string _WindowsProductType;
-		private readonly string _WindowsDomainName;
+		public SystemViewModel ViewModel { get; set; }
 
 		public SystemPage()
 		{
-			this._OperatingSystemContext = new OperatingSystemContext();
-
-			this._Hostname = _OperatingSystemContext.Hostname;
-			this._WindowsVersion = _OperatingSystemContext.WindowsVersion;
-			this._WindowsProductType = _OperatingSystemContext.WindowsProductType;
-			this._WindowsDomainName = _OperatingSystemContext.WindowsDomainName;
-
 			this.InitializeComponent();
+
+			ViewModel = new SystemViewModel();
 		}
-
-		public string Hostname { get { return this._Hostname; } }
-
-		public string WindowsVersion { get { return this._WindowsVersion; } }
-
-		public string WindowsProductType { get { return this._WindowsProductType; } }
-
-		public string WindowsDomainName { get { return this._WindowsDomainName; } }
 	}
 }
